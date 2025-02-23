7749aeb510917c9e04831162dbdb33e5e0d13706
Samandar-Komilov <komilov.samandar07@gmail.com>
2025-02-23 13:06:25 +0500

    FileManager class

---
+++ LinkedPlayM/music_player/file_manager.py
@@ -0,0 +1,17 @@
+import pathlib
+
+from . import Playlist
+
+
+class FileManager:
+    def load_songs_from_folder(self, folder_path: str) -> None:
+        """Scan a folder and return a list of valid song file paths."""
+
+    def save_playlist(self, playlist_name: str, songs: list[str]) -> None:
+        """Save a playlist (list of song file paths) to a JSON file."""
+
+    def load_playlist(self, playlist_name: str) -> list[str]:
+        """Load a playlist from a JSON file and return song paths."""
+
+    def is_valid_audio_file(self, path: str) -> bool:
+        """Check if a file is a valid audio format (MP3, WAV, etc.)."""
